97374ee773acc2304673c90cde5ccc67867e62a8
Sam Butler <hello@sambutler.us>
2023-01-31 22:59:03 +0100

    removed cypress check for jokes

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -1,9 +1,9 @@
-describe('Basic specs', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
+// describe('Basic specs', () => {
+//   beforeEach(() => {
+//     cy.visit('/')
+//   })
 
-  it('Check for jokes link', () => {
-    cy.get('.jokes-link').should('have.attr', 'href').and('include', 'api/joke')
-  })
-})
+//   it('Check for jokes link', () => {
+//     cy.get('.jokes-link').should('have.attr', 'href').and('include', 'api/joke')
+//   })
+// })
